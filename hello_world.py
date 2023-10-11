ffe615cc336baf24d8b739d989ad9c71597cfb17
SipuliCodes <vepsalainensisu@gmail.com>
2023-10-11 18:37:35 +0300

    Greeting based on the time of the day

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+from datetime import datetime
+
+current_hour = datetime.now().hour
+
+if 5 <= current_hour <= 10:
+    print("Good morning, World!")
+elif 10 < current_hour <= 12:
+    print("Hello World!")
+elif 12 < current_hour < 18:
+    print("Good afternoon, World!")
+elif 18 <= current_hour < 21.00:
+    print("Good Evening, World!")
+elif 21 <= current_hour <= 23:
+    print("Good Night, World!")
+else:
+    print("You should be sleeping")
